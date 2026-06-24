920db06c231fcf4d5633e072a5dfc4a18314da93
FirepigJay <djfaltgame@gmail.com>
2026-06-24 21:25:40 +0200

    added convert textnode to htmlnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -12,6 +12,12 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node3, node4)
         self.assertNotEqual(node2, node5)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -24,3 +25,27 @@ class TextNode:
     def __repr__(self) -> str:
         string = f'TextNode({self.text}, {self.text_type}, {self.url})'
         return string
+    
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    tag = None
+    prop = None
+    value = None
+    if text_node.text_type == TextType.TEXT:
+        value = text_node.text
+    if text_node.text_type == TextType.BOLD:
+        tag = 'b'
+        value = text_node.text
+    if text_node.text_type == TextType.ITALIC:
+        tag = 'i'
+        value = text_node.text
+    if text_node.text_type == TextType.CODE:
+        tag = 'code'
+        value = text_node.text
+    if text_node.text_type == TextType.LINK:
+        tag = 'a'
+        prop = {'href':text_node.url}
+    if text_node.text_type == TextType.IMAGE:
+        tag = 'img'
+        prop = {'src': text_node.url, 'alt': text_node.text}
+    
+    return LeafNode(tag = tag, value = value, props = prop)
