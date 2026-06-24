430695de1d2613bcf737f9658537f816598e09b3
FirepigJay <djfaltgame@gmail.com>
2026-06-24 08:00:49 +0200

    added TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
+        
+    def __repr__(self) -> str:
+        string = f'TextNode({self.text}, {self.text_type}, {self.url})'
+        return string
